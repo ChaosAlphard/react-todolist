abd1b505e4535f8df6d8c0796fb5fc52accff3a4
ChaosAlphard <Alphard450@outlook.com>
2019-08-30 23:02:08 +0800

    add todo-add method

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      list: [],
+      inputVal: ''
+    }
+  }
+
+  inputChange(e) {
+    this.setState({
+      inputVal: window.event.target.value
+    })
+    // e.target === window.event.target
+    console.log(e.target, window.event.target)
+  }
+
+  addTodo() {
+    if(this.state.inputVal.trim().length < 1) {
+      return false
+    }
+    this.setState({
+      list: [...this.state.list, this.state.inputVal],
+      inputVal: ''
+    })
+  }
+
+  render() {
+    return (
+      <div className="todo-list">
+        {/* todo */}
+        <div className="add-todo">
+          <input type="text" value={this.state.inputVal}
+            // this会指向调用它的对象(button)
+            // 需要使用bind 来改变this指向
+          onChange={this.inputChange.bind(this)}/>
+
+          <button type="button"
+          onClick={this.addTodo.bind(this)}>
+            添加
+          </button>
+        </div>
+        {/* body */}
+        <ul className="todo-list">{
+          this.state.list.map((item, i) => {
+            return (
+              <li className="todo-item"
+                key={i+item}>
+                {item}
+              </li>
+            )
+          })
+        }</ul>
+      </div>
+    )
+  }
 }
 
 export default App;
